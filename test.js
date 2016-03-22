162a89f7e1fca460d2a0d265ec92853f75d1e4be
tunnckoCore <mameto_100@mail.bg>
2016-03-22 19:22:34 +0200

    test: should return true if spawn options.stdio:inherit

--- test.js
+++ test.js
@@ -17,20 +17,26 @@ var isChildProcess = require('./index')
 
 test('should return `true` for `cp.exec`', function (done) {
   var exec = cp.exec('echo "hello world"')
-  test.equal(isChildProcess(exec), true)
+  test.strictEqual(isChildProcess(exec), true)
   done()
 })
 
 test('should return `true` for `cp.spawn`', function (done) {
   var spawn = cp.spawn('echo', ['hello world'])
-  test.equal(isChildProcess(spawn), true)
+  test.strictEqual(isChildProcess(spawn), true)
+  done()
+})
+
+test('should return `true` if spawn options.stdio is `inherit`', function (done) {
+  var res = cp.spawn('echo', ['hi'], {stdio: 'inherit'})
+  test.strictEqual(isChildProcess(res), true)
   done()
 })
 
 test('should return `false` for other emitters and streams', function (done) {
   var emitter = new Dush()
   var stream = fs.createReadStream('package.json')
-  test.equal(isChildProcess(emitter), false)
-  test.equal(isChildProcess(stream), false)
+  test.strictEqual(isChildProcess(emitter), false)
+  test.strictEqual(isChildProcess(stream), false)
   done()
 })
